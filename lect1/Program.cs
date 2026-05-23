f4aef2813584d1b959a5255c40f1fef9f8cb2cf3
LanaNew <l4n.steen@gmail.com>
2026-05-23 19:56:04 +0400

    Add project files.

---
+++ lect1/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace lect1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+
+
+
+
+
+			//Console.WriteLine("Hello, World!");  /// console.log  
+
+			//Console.ReadLine();       //prompt 
+
+
+
+			//0 255    
+
+
+
+
+			//byte age = 50;
+			//Console.WriteLine(age);
+
+
+			//int point = 1000;
+
+			//point = 2000;
+
+			//float pi = 3.14f;
+			//double score = 1000.5;
+			//decimal price = 100.5m;  /// MONEY
+
+
+
+			//char ch = 'a';
+			//string str = "dsdffd";
+
+
+			//bool isvalid = false;
+
+
+			//Console.WriteLine("Enter your name");
+
+			//string name = Console.ReadLine();  /////////////// string    "50"
+
+
+			//Console.WriteLine("your name is " + name);
+
+
+			//string userInputAge = Console.ReadLine();  // "50"
+
+			//byte useAge = byte.Parse(userInputAge);  //byte  50
+
+
+			//int intAge = int.Parse(userInputAge);  // int 50
+
+
+
+			//num1 num2 
+			string userinputage1 = Console.ReadLine();
+			string userinputage2 = Console.ReadLine();
+			byte userage1 = byte.Parse(userinputage1);
+			byte userage2 = byte.Parse(userinputage2);
+			Console.WriteLine(userage1 + userage2);
+
+
+
+
+
+			Console.WriteLine();
+
+
+
+
+
+
+
+			/*
+			dadada
+
+			dsdsd
+
+			*/
+
+			/*
+			//LocalDataStoreSlot  
+
+			//	dssd 
+			*/
+
+
+
+			#region regitration 
+
+
+			//dsdfgf
+
+			#endregion
+
+
+
+		}
+	}
+}
